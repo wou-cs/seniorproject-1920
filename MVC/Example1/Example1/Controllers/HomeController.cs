7d52366565c1aac5a435ee7b02292288d3cc5bbf
Scot Morse <morses@mash.wou.edu>
2019-10-21 14:04:36 -0700

    add example from class 10/21/2019 on intro to MVC

---
+++ MVC/Example1/Example1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET /Home/Index
+        // GET /Home
+        // Get /
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET /Home/About
+        public ActionResult About()
+        {
+            Debug.WriteLine(Request["value"]);
+            Debug.WriteLine(Request["data"]);
+            Debug.WriteLine(Request["data2"]);
+            Double.TryParse(Request["data2"], out double data2);
+
+            ViewBag.Message = "Your application description page.";
+            ViewData["Message"] = "some string";
+            int a = 20;
+            int b = 2;
+            double c = a + data2;
+            ViewBag.C = c;
+
+            return View();
+        }
+
+        // GET /Home/Contact
+        public ActionResult Contact(double? data2)
+        {
+            ViewBag.Message = "Your contact page.";
+            if(data2.HasValue)
+            {
+                ViewBag.Answer = data2;
+            }
+            else
+            {
+                ViewBag.Answer = 92.3;
+            }
+            return View();
+        }
+    }
+}
---
+++ MVC/Example1/Example1/Controllers/TestAFormController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example1.Controllers
+{
+    public class TestAFormController : Controller
+    {
+        // GET: TestAForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Form1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form1(int? numA)
+        {
+            ViewBag.ThankYou = "Thank you for your submission";
+            ViewBag.NumA = (int)numA + 10;
+            return View();
+        }
+    }
+}
