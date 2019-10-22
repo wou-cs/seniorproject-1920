1f235e824c727139bcbaa4f361d4d95f78b90424
Scot Morse <morses@mash.wou.edu>
2019-10-22 14:00:03 -0700

    add MVC intro example from class on Tuesday 10_22

---
+++ MVC/Example1a/Example1a/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example1a.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ~/User/CreateMessage
+        [HttpGet]
+        public ActionResult CreateMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateMessage(string name, string email, string message)
+        {
+            ViewBag.name = name;
+            ViewBag.email = email;
+            ViewBag.message = message;
+
+            return View();
+        }
+    }
+}
