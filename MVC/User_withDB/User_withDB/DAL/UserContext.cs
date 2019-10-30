ed3f54167d2c40e21d8000e46c76e1d644815c38
Scot Morse <morses@wou.edu>
2019-10-30 13:02:01 -0700

    update model and add context class to withDB example, doing this now so we can show what adding Entity Framework to our project does

---
+++ MVC/User_withDB/User_withDB/DAL/UserContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using User_withDB.Models;
+
+namespace User_withDB.DAL
+{
+    public class UserContext : DbContext
+    {
+        // UserDB is the name of the database (in a config file)
+        public UserContext() : base("name=UserDB")
+        {
+
+        }
+
+        // "Users" is the name of the table to access
+        // <User> is the entity that resides in each row of the table
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
--- MVC/User_withDB/User_withDB/Models/User.cs
+++ MVC/User_withDB/User_withDB/Models/User.cs
@@ -5,10 +5,21 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+/*
+ * Needs to match our db:
+ * [ID]		INT IDENTITY (1,1)	NOT NULL,
+	[FirstName] NVARCHAR(64)		NOT NULL,
+	[LastName]	NVARCHAR(128)		NOT NULL,
+	[DOB]		DATETIME			NOT NULL,
+	CONSTRAINT [PK_dbo.Users] PRIMARY KEY CLUSTERED ([ID] ASC)
+ */
+
 namespace User_WithDB.Models
 {
     public class User
     {
+        // property named: ID, Id, or UserID, or maybe UserId is automatically
+        // mapped to our ID primary key, by our ORM (Object Relational Mapper) Entity Framework
         [Key]
         public int ID { get; set; }
 
